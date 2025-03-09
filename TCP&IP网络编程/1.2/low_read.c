fd5aadd392b33f0598436715bbf88859fd8d7921
LAN40 <sdfk32@outlook.com>
2025-03-09 17:26:42 +0800

    1

--- TCP&IP网络编程/1.2/low_read.c
+++ TCP&IP网络编程/1.2/low_read.c
@@ -7,5 +7,24 @@ void error_handling(char* message);
 
 int main(void)
 {
-    
+    int fd;
+    char buf[BUF_SIZE];
+
+    fd=open("data.txt", O_RDONLY);
+    if (fd==-1)
+        error_handling("open() error!");
+    printf("file decriptor: %d \n", fd);
+
+    if (read(fd, buf, sizeof(buf))==-1)
+        error_handling("read() error!");
+    printf("file data: %s", buf);
+    close(fd);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
