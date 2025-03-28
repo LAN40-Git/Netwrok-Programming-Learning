c7d343f4def1da66201d7b23f24aa914589afbd2
LAN40 <sdfk32@outlook.com>
2025-03-28 10:44:27 +0800

    添加信号处理和进程管理示例，包含waitpid、信号处理和去除僵尸进程的实现

---
+++ TCP&IP网络编程/10.3/sigaction.c
@@ -0,0 +1,30 @@
+#define _POSIX_C_SOURCE 199309L
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void timeout(int sig)
+{
+    if (sig == SIGALRM)
+        puts("Time out!");
+    alarm(2);
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    struct sigaction act;
+    act.sa_handler = timeout;
+    sigemptyset(&act.sa_mask);
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGALRM, &act, 0);
+
+    alarm(2);
+
+    for (i = 0; i < 3; i++) {
+        puts("wait...");
+        sleep(100);
+    }
+    return 0;
+}
