f180313ebe6e3fb4d87b3274a43fe4dd3b031bb0
LAN40 <sdfk32@outlook.com>
2025-03-31 10:43:13 +0800

    修复echo_selectserv.c中的bind函数返回值检查，确保正确处理客户端连接请求

--- TCP&IP网络编程/12.2/echo_selectserv.c
+++ TCP&IP网络编程/12.2/echo_selectserv.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -0)
+    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("bind() error!");
     if (listen(serv_sock, 5) == -1)
         error_handling("listen() error!");
@@ -52,24 +52,22 @@ int main(int argc, char *argv[])
         
         for (i = 0; i < fd_max+1; i++) {
             if (FD_ISSET(i, &cpy_reads)) {
-                if (i == serv_sock) {
+                if (i == serv_sock) { // 如果是服务端套接字文件描述符，说明有客户端连接请求
                     addr_sz = sizeof(clnt_addr);
                     clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &addr_sz);
-                    if (clnt_sock == -1)
-                        continue;
                     FD_SET(clnt_sock, &reads);
                     if (fd_max < clnt_sock)
                         fd_max = clnt_sock;
-                        printf("connected client: %d \n", clnt_sock);
+                    printf("connected client: %d \n", clnt_sock);
                 }
-                else {
+                else { // 如果是客户端套接字文件描述符，则读取客户端发送的数据
                     str_len = read(i, buf, BUF_SIZE);
-                    if (str_len == 0) {
+                    if (str_len == 0) { // 接收的数据为EOF时需要断开连接，并从reads中删除相应信息
                         FD_CLR(i, &reads);
                         close(i);
                         printf("closed client: %d \n", i);
                     }
-                    else {
+                    else { // 接收的数据为字符串时，需要执行回声服务
                         write(i, buf, str_len);
                     }
                 }
