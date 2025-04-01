7a9a8eb540b8e63c7b04327453960c4330aab354
LAN40 <sdfk32@outlook.com>
2025-04-01 11:48:06 +0800

    添加news_sender_brd.c和news_receiver_brd.c示例程序，演示基于UDP的广播消息发送和接收；添加news.txt文件作为消息源

---
+++ TCP&IP网络编程/14.2/news_sender_brd.c
@@ -0,0 +1,52 @@
+#define BUF_SIZE 30
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+    int send_sock;
+    struct sockaddr_in broad_addr;
+    FILE *fp;
+    char buf[BUF_SIZE];
+    int bcast = 1;
+
+    if (argc != 3) {
+        printf("Usage : %s <Broadcast IP> <PORT>\n", argv[0]);
+        exit(1);
+    }
+
+    send_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    if (send_sock == -1)
+        error_handling("socket() error!");
+    
+    memset(&broad_addr, 0, sizeof(broad_addr));
+    broad_addr.sin_family = AF_INET;
+    broad_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    broad_addr.sin_port = htons(atoi(argv[2]));
+
+    setsockopt(send_sock, SOL_SOCKET, SO_BROADCAST, (void*)&bcast, sizeof(bcast));
+    if ((fp=fopen("news.txt", "r")) == NULL)
+        error_handling("fopen() error!");
+    
+    while(fgets(buf, BUF_SIZE, fp) != NULL) {
+        sendto(send_sock, buf, strlen(buf), 0, (struct sockaddr*)&broad_addr, sizeof(broad_addr));
+        sleep(2);
+    }
+    fclose(fp);
+    close(send_sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
