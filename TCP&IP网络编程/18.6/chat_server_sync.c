979b322239ee56f272b774c7bb9ba34d1e7c5288
LAN40 <sdfk32@outlook.com>
2025-04-06 22:08:44 +0800

    Implement multi-threaded chat server with synchronization
    - Added chat_server.c: A multi-threaded chat server that handles multiple clients using pthreads.
    - Implemented client handling in separate threads with mutexes for thread safety.
    - Added functions for sending messages to all connected clients and error handling.
    - Introduced chat_server_sync.c: A synchronized version of the chat server that uses a single mutex for message handling.
    - Improved client connection management and message broadcasting.

--- TCP&IP网络编程/18.6/chat_server_sync.c
+++ TCP&IP网络编程/18.6/chat_server_sync.c
@@ -14,7 +14,9 @@ void error_handling(char *message);
 
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
+char msg[BUF_SIZE];
 pthread_mutex_t mutex;
+pthread_mutex_t mutex_message;
 
 int main(int argc, char *argv[])
 {
@@ -28,6 +30,7 @@ int main(int argc, char *argv[])
     }
 
     pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&mutex_message, NULL);
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
     if (serv_sock == -1)
         error_handling("socket() error!");
@@ -51,9 +54,53 @@ int main(int argc, char *argv[])
         clnt_socks[clnt_cnt++] = clnt_sock;
         pthread_mutex_unlock(&mutex);
 
-        pthread_create(&t_id, NULL, handle_clnt, (void*)clnt_sock);
+        pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
         pthread_detach(t_id);
         printf("Connected client IP: %s \n", inet_ntoa(clnt_addr.sin_addr));
     }
+    close(serv_sock);
     return 0;
+}
+
+void *handle_clnt(void *arg)
+{
+    int clnt_sock = *((int*)arg);
+    int str_len = 0;
+
+    pthread_mutex_lock(&mutex_message);
+    while((str_len=read(clnt_sock, msg, BUF_SIZE)) != 0) {
+        send_msg(msg, str_len);
+    }
+    pthread_mutex_unlock(&mutex_message);
+
+    pthread_mutex_lock(&mutex);
+    // 移除客户端连接
+    for (int i = 0; i < clnt_cnt; i++) {
+        if (clnt_sock == clnt_socks[i]) {
+            while(i++ < clnt_cnt-1)
+                clnt_socks[i] = clnt_socks[i+1];
+            break;
+        }
+    }
+    clnt_cnt--;
+    pthread_mutex_unlock(&mutex);
+    printf("Closed client: %d \n", clnt_sock);
+    close(clnt_sock);
+    return NULL;
+}
+
+void send_msg(char *msg, int len)
+{
+    pthread_mutex_lock(&mutex);
+    for (int i = 0; i < clnt_cnt; i++) {
+        write(clnt_socks[i], msg, len);
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
