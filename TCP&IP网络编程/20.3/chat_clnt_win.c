8c4bbf910618ce078d2e3d3c45a14abf09f0a6c8
“LAN40” <sdfk32@outlook.com>
2025-04-07 09:48:10 +0800

    实现多线程聊天客户端和服务器，添加消息发送和接收功能

---
+++ TCP&IP网络编程/20.3/chat_clnt_win.c
@@ -0,0 +1,90 @@
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <process.h>
+
+unsigned WINAPI SendMsg(void *arg);
+unsigned WINAPI RecvMsg(void *arg);
+void ErrorHandling(char *msg);
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+int main(int argc, char *argv[])
+{
+    WSADATA wsaData;
+    SOCKET sock;
+    SOCKADDR_IN servAddr;
+    HANDLE hSndThread, hRcvThread;
+    if (argc != 4) {
+        printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+        exit(1);
+    }
+
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+        ErrorHandling("WSAStartup() error!");
+
+    sprintf(name, "[%s]", argv[3]);
+    
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+        ErrorHandling("socket() error!");
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+    servAddr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) == -1)
+        ErrorHandling("connect() error!");
+
+    hSndThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&sock, 0, NULL);
+    hRcvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&sock, 0, NULL);
+    WaitForSingleObject(hSndThread, INFINITE);
+    WaitForSingleObject(hRcvThread, INFINITE);
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
+
+unsigned WINAPI SendMsg(void *arg)
+{
+    SOCKET sock = *((SOCKET*)arg);
+    char nameMsg[NAME_SIZE+BUF_SIZE];
+    while(1) {
+        fgets(msg, BUF_SIZE, stdin);
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+            closesocket(sock);
+            exit(0);
+        }
+        sprintf(nameMsg, "%s %s", name, msg);
+        send(sock, nameMsg, sizeof(nameMsg), 0);
+    }
+    return 0;
+}
+
+unsigned WINAPI RecvMsg(void *arg)
+{
+    SOCKET sock = *((SOCKET*)arg);
+    char nameMsg[NAME_SIZE+BUF_SIZE];
+    int strLen;
+    while(1) {
+        strLen = recv(sock, nameMsg, BUF_SIZE+NAME_SIZE-1, 0);
+        if (strLen == -1)
+            return -1;
+        nameMsg[strLen] = 0;
+        fputs(nameMsg, stdout);
+    }
+    return 0;
+}
+
+void ErrorHandling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
--- TCP&IP网络编程/20.3/chat_serv_win.c
+++ TCP&IP网络编程/20.3/chat_serv_win.c
@@ -1,7 +1,97 @@
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <process.h>
+
+unsigned WINAPI HandleClnt(void *arg);
+void SendMsg(char *arg, int len);
+void ErrorHandling(char *msg);
+
+int clntCnt = 0;
+SOCKET clntSocks[MAX_CLNT];
+HANDLE hMutex;
 
 int main(int argc, char *argv[])
 {
+    WSADATA wsaData;
+    SOCKET hServSock, hClntSock;
+    SOCKADDR_IN servAddr, clntAddr;
+    int clntAddrSz;
+    HANDLE hThread;
+    if (argc != 2) {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+        ErrorHandling("WSAStartup() error!");
     
+    hMutex = CreateMutex(NULL, FALSE, NULL);
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (hServSock == -1)
+        ErrorHandling("socket() error!");
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(hServSock, (struct sockaddr*)&servAddr, sizeof(servAddr)) == -1)
+        ErrorHandling("bind() error!");
+    if (listen(hServSock, 5) == -1)
+        ErrorHandling("listen() error!");
+
+    while(1) {
+        clntAddrSz = sizeof(clntAddr);
+        hClntSock = accept(hServSock, (struct sockaddr*)&clntAddr, &clntAddrSz);
+        if (hClntSock == -1)
+            ErrorHandling("accept() error!");
+        WaitForSingleObject(hMutex, INFINITE);
+        clntSocks[clntCnt++] = hClntSock;
+        ReleaseMutex(hMutex);
+
+        hThread = (HANDLE)_beginthreadex(NULL, 0, HandleClnt, (void*)&hClntSock, 0, NULL);
+        printf("Connected client IP: %s \n", inet_ntoa(clntAddr.sin_addr));
+    }
+    closesocket(hServSock);
+    WSACleanup();
     return 0;
+}
+
+unsigned WINAPI HandleClnt(void *arg)
+{
+    SOCKET hClntSock = *((SOCKET*)arg);
+    int strLen = 0;
+    char msg[BUF_SIZE];
+
+    while((strLen=recv(hClntSock, msg, sizeof(msg), 0)) != 0)
+        SendMsg(msg, strLen);
+
+    WaitForSingleObject(hMutex, INFINITE);
+    for (int i = 0; i < clntCnt; i++) {
+        if (hClntSock == clntSocks[i]) {
+            while(i++ < clntCnt-1) {
+                clntSocks[i] = clntSocks[i+1];
+            }
+            break;
+        }
+    }
+}
+
+void SendMsg(char *msg, int len)
+{
+    WaitForSingleObject(hMutex, INFINITE);
+    for (int i = 0; i < clntCnt; i++)
+        send(clntSocks[i], msg, len, 0);
+    ReleaseMutex(hMutex);
+}
+
+void ErrorHandling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
